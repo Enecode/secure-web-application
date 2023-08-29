8be12049f83826e3d666ab2117c9abe572b0b730
Isah Jacob <isahjacob0@gmail.com>
2023-08-30 00:45:48 +0100

    added env file to store secret key

--- web_sec_project/settings.py
+++ web_sec_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#as5u$+u$mf&cd7-5bonn5vm@x$rdv@gt7_7zmg7c&0(j$#q(k'
+SECRET_KEY = environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
